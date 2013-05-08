f6fd00392997ff0210129a7f5faa2e7fd9e71d07
Justin Leveck <jleveck@gmail.com>
2013-05-08 23:00:22 +0000

    cleaned up deprecations

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ticketee::Application.config.secret_token = 'f44af97deebfec0d44d88c74012d2f7d26874634476f1b19251a9e1337c8025c40ac31a3548c95787747b9de29f0d6d647f8d7ac3b664c22f0cae8dadad69241'
+Ticketee::Application.config.secret_key_base = 'f44af97deebfec0d44d88c74012d2f7d26874634476f1b19251a9e1337c8025c40ac31a3548c95787747b9de29f0d6d647f8d7ac3b664c22f0cae8dadad69241'
